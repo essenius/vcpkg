451329544cd53cbb754bce72e290f278cea979ab
Rik Essenius <essenius@users.noreply.github.com>
2023-02-11 16:10:21 +0100

    Create portfile.cmake

---
+++ ports/safe-cstring/portfile.cmake
@@ -0,0 +1,13 @@
+# header-only library
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO essenius/safe-cstring
+    REF 
+    SHA512 
+    HEAD_REF master
+)
+
+file(INSTALL "${SOURCE_PATH}/SaveCString.h"  DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
