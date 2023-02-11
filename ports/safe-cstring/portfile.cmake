9083f860b21b8d92fe18d40f844a6448e310427c
Rik Essenius <essenius@users.noreply.github.com>
2023-02-11 17:46:19 +0100

    corrected REF and SHA512

--- ports/safe-cstring/portfile.cmake
+++ ports/safe-cstring/portfile.cmake
@@ -3,10 +3,11 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO essenius/safe-cstring
-    REF c226a79c9cd9070dafc3b341c120b6e3f819ba11
+    REF 72f1aa130b807fc7c4e2968bbfac584d0f455048
+    SHA512 0e58de509461a82c02cd955eb10e34fc1352a739e12422427fbf06e3334e4203ebafe63eb36d8e576a2dcb53b31ef1119d6615878a24ed03e316ad0ebfecdb65
     HEAD_REF main
 )
 
-file(INSTALL "${SOURCE_PATH}/SaveCString.h"  DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(INSTALL "${SOURCE_PATH}/SafeCString/SafeCString.h"  DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
