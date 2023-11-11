286acb217b6252910b74ecb6d019aabe0b19d2e4
Rik Essenius <essenius@users.noreply.github.com>
2023-11-11 12:40:40 +0100

    sha512 added

--- ports/safe-cstring/portfile.cmake
+++ ports/safe-cstring/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO essenius/safe-cstring
     REF 081e8eb4903ba4191464afedd7306c6eaf2ddac6
-    SHA512 0
+    SHA512 56789654664097b790fbcc77587e599e3d6e92f26b32019e034fb6ea9eeb6c4a18759bbfbdebc9200359d96cf157cce7b6c6f86a42f670e168c2ade3d826b48a
     HEAD_REF main
 )
 
