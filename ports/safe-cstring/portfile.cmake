19c7ae88bf0332d4372657041faf635f56e8b47f
Rik Essenius <essenius@users.noreply.github.com>
2023-11-11 16:21:42 +0100

    replaced git-tree by tag for 0.6.3

--- ports/safe-cstring/portfile.cmake
+++ ports/safe-cstring/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO essenius/safe-cstring
-    REF 081e8eb4903ba4191464afedd7306c6eaf2ddac6
+    REF 0.6.3
     SHA512 240bc8030fc1882a13e493c22a11e4e4ce8b36a1cee5e46c409271fe0d01987265c53e0faabdabf0896d31c9618a3e0d6f190826c37c32513107da0c96eba1f2
     HEAD_REF main
 )
