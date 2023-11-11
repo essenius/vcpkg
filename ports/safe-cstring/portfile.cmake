7a33298cfe46a0babfbf103f37ee30544170c6d9
Rik Essenius <essenius@users.noreply.github.com>
2023-11-11 12:10:15 +0100

    bump to safe-cstring 6.3

--- ports/safe-cstring/portfile.cmake
+++ ports/safe-cstring/portfile.cmake
@@ -3,8 +3,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO essenius/safe-cstring
-    REF 5f73eef218e8e6e95cc1c2969da3561454d5d3b0
-    SHA512 7c2c5f4c0e0461a0ca3fdfb2268846cb29401ab546aefe6ab322de69e30d9fea10a2960903e08847e63a3ea5da5413e8b3f85e7dc94cf5da96c68b8935c92c59
+    REF 081e8eb4903ba4191464afedd7306c6eaf2ddac6
+    SHA512 0
     HEAD_REF main
 )
 
