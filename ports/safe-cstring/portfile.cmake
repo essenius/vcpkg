e1df76bb3b7ff55a746aff82f6881d79de90ac90
Rik Essenius <essenius@users.noreply.github.com>
2023-11-11 17:20:22 +0100

    corrected install folder

--- ports/safe-cstring/portfile.cmake
+++ ports/safe-cstring/portfile.cmake
@@ -8,5 +8,5 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
-file(INSTALL "${SOURCE_PATH}/SafeCString/src/SafeCString.h"  DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(INSTALL "${SOURCE_PATH}/src/SafeCString.h"  DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
