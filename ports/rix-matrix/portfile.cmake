f15e87a24f8789bd358c2e30ede7587adc2b1025
Rik Essenius <essenius@users.noreply.github.com>
2023-11-12 15:42:20 +0100

    Corrected SHA512 for rix-matrix

--- ports/rix-matrix/portfile.cmake
+++ ports/rix-matrix/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO essenius/Matrix
     REF "${VERSION}"
-    SHA512 0
+    SHA512 3a76fc52d68cab4cceb3199f8a309f6c020bf5316ff75dc56ab3428decacbb489439c2fb0b3b72ca53cc4938aa567e5ba6c88cf1cdf13bf469005f64aafafe1e
     HEAD_REF main
 )
 
