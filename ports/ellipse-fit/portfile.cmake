e65f780b5db8a67564a8a8d8bf1e54af327983ab
Rik Essenius <essenius@users.noreply.github.com>
2023-12-05 16:42:29 +0100

    updated rix-matrix and ellipse-fit

--- ports/ellipse-fit/portfile.cmake
+++ ports/ellipse-fit/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO essenius/EllipseFit
-    REF "c715613abc2c78796980e09514c8d5836fa208d1"
+    REF "cbcd2e8cef3741697d38e7e523a4cdd0940df53a"
     SHA512 9890453b005c0aab6a2fa2114da758ee58853ea28cbbb2acf4c91a89ce0cbfa45828af9feaa80b9f462aa62d62f5cf226da6ce41f84da5ab258dce05d078e7d6
     HEAD_REF main
 )
