5ea3f90c7492eb871196ac81c24980445523393f
Rik Essenius <essenius@users.noreply.github.com>
2023-12-05 19:34:52 +0100

    newer version

--- ports/rix-matrix/portfile.cmake
+++ ports/rix-matrix/portfile.cmake
@@ -7,7 +7,7 @@ vcpkg_from_github(
     REPO essenius/Matrix
     #REF "${VERSION}"
     REF "26db113c6cdfaf66d8d25b49b7715f45f8efa1a7"
-    SHA512 495d10639ef611dd41ab2589a7e4a67e7f02691fbb96742eb3e2e5a85efb8cfb4382f95c0cfdc98e238739666745568363f43b1a8b7ba2db5c12858147b7ae0e
+    SHA512 f3193d18db2d2557eba0a9d4e9393cadca068a86affcedf7f5b86b0ab9b42abd862937d3baa0379b84ed5b0dd6c47fdb281bf646b1474fc9a55f664d1700fe90
     HEAD_REF main
 )
 
