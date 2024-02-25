cb5e9428b64fbf2cb20c7dd6e06631d57c99e50e
essenius <essenius@users.noreply.github.com>
2024-02-25 20:11:49 +0530

    Updated esp32-mock and Matrix for better vcpkg detection in cmake

--- ports/esp32-mock/portfile.cmake
+++ ports/esp32-mock/portfile.cmake
@@ -3,17 +3,25 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO essenius/esp32-mock
-    REF "07307e776ecec38746623ad6ff6d7034f2a02a52"
-    SHA512 38c3f68c838e7996bb328cb69deb44e633dfbcebbbe586babe8d7e05e1ee73109598816d59d096f0146869c1e08e86831f5533f6a9ad0f090817e42596d2166d
+    REF "c8388ea024a6efa2a97ddaca9cf8cbf3dcacf9ca"
+    SHA512 d562676d5feacac4c1e708a365cab8518f2ef82e0ac9026eefea5179ad6f4db19359c21597494c9a333d79d1a15503b2fd2d6c04fd0a91a7e3e3f8900f0c4ef5
     HEAD_REF main
 )
+
 
 vcpkg_cmake_configure( 
     SOURCE_PATH "${SOURCE_PATH}" 
 )
 
+get_cmake_property(_variableNames VARIABLES)
+
+list (SORT _variableNames)
+foreach (_variableName ${_variableNames})
+    message(STATUS "p: ${_variableName}=${${_variableName}}\n")
+endforeach()
+
 vcpkg_cmake_install()
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
