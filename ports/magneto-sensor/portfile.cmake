f6ec88d76ff1c32f796ad60a1fe502b9ce4aebb2
essenius <essenius@users.noreply.github.com>
2024-03-11 13:07:11 +0100

    magneto-sensor to v0.0.4

--- ports/magneto-sensor/portfile.cmake
+++ ports/magneto-sensor/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO essenius/MagnetoSensor
-    REF "v0.0.3"
+    REF "v0.0.4"
     SHA512 ae024f47e712e12ad44eadd227a1c49666c3b3497d9ea648f8696ed2416afd11692083db5a067d6544252ac5455a16d2283bc4a713e23072a376e9b6c94d9c4f
     HEAD_REF main
 )
