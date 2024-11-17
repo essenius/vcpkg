245c61562b8769d4854d2a9a3098f93396813e36
Rik Essenius <essenius@users.noreply.github.com>
2024-11-17 22:57:33 +0100

    EllipseFit v0.2.4

--- ports/ellipse-fit/portfile.cmake
+++ ports/ellipse-fit/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO essenius/EllipseFit
-    REF "v0.2.3"
-    SHA512 4cdd25559a84f14deeacc9a7a8dc61c42c0a80ddbcf7652778dc6e2f4d39a511c4cd5ff6f9767098a9d7528049df249b86ae19ca39962b12685b88638d003144
+    REF "v0.2.4"
+    SHA512 cb4b312035fda1c1843edc3deb89aeac8c0090630db41feced95d3235858d2a5d6501e335ba6342aea6d0d61218391430ddd94c38f79aee90f23a042579df695
     HEAD_REF main
 )
 
