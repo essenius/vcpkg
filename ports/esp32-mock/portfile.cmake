3253b2b7cadd3c20e521308234fcc78f383f3593
essenius <essenius@users.noreply.github.com>
2025-12-28 10:46:03 +0100

    esp-mock v0.1.15

--- ports/esp32-mock/portfile.cmake
+++ ports/esp32-mock/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO essenius/esp32-mock
-    REF "v0.1.12"
-    SHA512 e1059f3a30e5f7bef1bddbc2fd2a7601647390ef2e56a31423a931553cce03a82abff0ae40a38c722a14a9c27248144b7bb27784f9846bd1fb25da12660c978f
+    REF "v0.1.15"
+    SHA512 15c52c7d44276acb072ea3bcdbe7270fc2aad22aef899c862fa5b09f1fa138c50d1857cff6aa24195595d6926fead6df802caf60df1f0463ebbbd77022ec6455
     HEAD_REF main
 )
 
